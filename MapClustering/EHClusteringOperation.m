cdbcadb81451ef22edb516aaec709167890da482
Hani Ibrahim <hani.eloksh@vodafone.com>
2014-08-16 15:57:14 +0300

    adding try/catch block

--- MapClustering/EHClusteringOperation.m
+++ MapClustering/EHClusteringOperation.m
@@ -35,57 +35,62 @@
 - (void)main
 {
 	@autoreleasepool {
-		MKMapRect actualVisibleRect = [self.mapView visibleMapRect];
-		MKMapRect visibleRect = MKMapRectInset(actualVisibleRect, -actualVisibleRect.size.width/MAP_MARGIN, -actualVisibleRect.size.height/MAP_MARGIN);
-		NSMutableSet *newAnnotations = [NSMutableSet set];
-		
-		// Check if region is zoomed or not
-		BOOL sameWidth = fabs(visibleRect.size.width - self.mapView.previousMapRect.size.width) < 1e-6;
-		BOOL sameHeight = fabs(visibleRect.size.height - self.mapView.previousMapRect.size.height) < 1e-6;
-		BOOL sameZoomLevel = sameWidth && sameHeight;
-		if (sameZoomLevel) {
-			// Display all the previously displayed annotation for the current region, If the zoom level is not changed
-			NSSet *currentAnnotation = [self.mapView annotationsInMapRect:visibleRect];
-			[newAnnotations unionSet:currentAnnotation];
-		}
-		
-		// Divide the visibleRect into grids
-		CGFloat xGridCount = self.mapView.frame.size.width / GRID_STEP;
-		CGFloat yGridCount = self.mapView.frame.size.height / GRID_STEP;
-		double xGridStepSize = visibleRect.size.width / xGridCount;
-		double yGridStepSize = visibleRect.size.height / yGridCount;
-		
-		// Loop On each grid
-		for (int xStep = 0; xStep < xGridCount; xStep++) {
-			for (int yStep = 0; yStep < yGridCount; yStep++) {
-				// Return if operatino is cancelled
-				if ([self isCancelled]) {return;}
-				
-				// Get the Map Rect for the grid
-				MKMapRect gridRect = MKMapRectMake(visibleRect.origin.x + xStep * xGridStepSize, visibleRect.origin.y + yStep * yGridStepSize, xGridStepSize, yGridStepSize);
-				
-				// Get Grid Annotations to be display
-				// We will select only one Annotation to be displayed
-				NSSet *gridAnnotations = [self.mapView getAllAnnotationsInRect:gridRect];
-				
-				// Display the annotation only if the grid rect is not displayed before at the same zoom level
-				if (gridAnnotations.count > 0 && !(sameZoomLevel && MKMapRectIntersectsRect(self.mapView.previousMapRect, gridRect))) {
-					[newAnnotations addObject:[gridAnnotations anyObject]];
+		@try {
+			MKMapRect actualVisibleRect = [self.mapView visibleMapRect];
+			MKMapRect visibleRect = MKMapRectInset(actualVisibleRect, -actualVisibleRect.size.width/MAP_MARGIN, -actualVisibleRect.size.height/MAP_MARGIN);
+			NSMutableSet *newAnnotations = [NSMutableSet set];
+			
+			// Check if region is zoomed or not
+			BOOL sameWidth = fabs(visibleRect.size.width - self.mapView.previousMapRect.size.width) < 1e-6;
+			BOOL sameHeight = fabs(visibleRect.size.height - self.mapView.previousMapRect.size.height) < 1e-6;
+			BOOL sameZoomLevel = sameWidth && sameHeight;
+			if (sameZoomLevel) {
+				// Display all the previously displayed annotation for the current region, If the zoom level is not changed
+				NSSet *currentAnnotation = [self.mapView annotationsInMapRect:visibleRect];
+				[newAnnotations unionSet:currentAnnotation];
+			}
+			
+			// Divide the visibleRect into grids
+			CGFloat xGridCount = self.mapView.frame.size.width / GRID_STEP;
+			CGFloat yGridCount = self.mapView.frame.size.height / GRID_STEP;
+			double xGridStepSize = visibleRect.size.width / xGridCount;
+			double yGridStepSize = visibleRect.size.height / yGridCount;
+			
+			// Loop On each grid
+			for (int xStep = 0; xStep < xGridCount; xStep++) {
+				for (int yStep = 0; yStep < yGridCount; yStep++) {
+					// Return if operatino is cancelled
+					if ([self isCancelled]) {return;}
+					
+					// Get the Map Rect for the grid
+					MKMapRect gridRect = MKMapRectMake(visibleRect.origin.x + xStep * xGridStepSize, visibleRect.origin.y + yStep * yGridStepSize, xGridStepSize, yGridStepSize);
+					
+					// Get Grid Annotations to be display
+					// We will select only one Annotation to be displayed
+					NSSet *gridAnnotations = [self.mapView getAllAnnotationsInRect:gridRect];
+					
+					// Display the annotation only if the grid rect is not displayed before at the same zoom level
+					if (gridAnnotations.count > 0 && !(sameZoomLevel && MKMapRectIntersectsRect(self.mapView.previousMapRect, gridRect))) {
+						[newAnnotations addObject:[gridAnnotations anyObject]];
+					}
 				}
 			}
+			
+			// Get Current Annotations to remove
+			NSMutableSet *currentAnnotations = [NSMutableSet setWithArray:self.mapView.annotations];
+			[currentAnnotations minusSet:newAnnotations];
+			
+			// Add the Annotation to the map View
+			EHMapView *mapView = self.mapView;
+			dispatch_async(dispatch_get_main_queue(), ^{
+				[mapView removeAnnotations:[currentAnnotations allObjects]];
+				[mapView addAnnotations:[newAnnotations allObjects]];
+				mapView.previousMapRect = visibleRect;
+			});
 		}
-		
-		// Get Current Annotations to remove
-		NSMutableSet *currentAnnotations = [NSMutableSet setWithArray:self.mapView.annotations];
-		[currentAnnotations minusSet:newAnnotations];
-		
-		// Add the Annotation to the map View
-		EHMapView *mapView = self.mapView;
-		dispatch_async(dispatch_get_main_queue(), ^{
-			[mapView removeAnnotations:[currentAnnotations allObjects]];
-			[mapView addAnnotations:[newAnnotations allObjects]];
-			mapView.previousMapRect = visibleRect;
-		});
+		@catch (NSException *exception) {
+			
+		}
 	}
 }
 
